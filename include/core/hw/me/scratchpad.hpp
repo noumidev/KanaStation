e954069ad8013bb6ac54ac1520e32ffda1392a3e
noumi <139501014+noumidev@users.noreply.github.com>
2026-06-21 21:16:39 +0200

    [ME] Add EDRAM

---
+++ include/core/hw/me/scratchpad.hpp
@@ -0,0 +1,17 @@
+/*
+ * KanaStation is an experimental PlayStation Portable emulator.
+ * Copyright (C) 2026  noumidev
+ */
+
+/* core/hw/me/scratchpad.hpp - MediaEngine scratchpad */
+
+#pragma once
+
+namespace kanacore::hw::me::scratchpad {
+
+void initialize();
+void soft_reset();
+void hard_reset();
+void shutdown();
+
+};
---
+++ src/core/hw/me/scratchpad.cpp
@@ -0,0 +1,72 @@
+/*
+ * KanaStation is an experimental PlayStation Portable emulator.
+ * Copyright (C) 2026  noumidev
+ */
+
+/* core/hw/me/scratchpad.cpp - MediaEngine scratchpad */
+
+#include <core/hw/me/scratchpad.hpp>
+
+#include <array>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+#include <common/types.hpp>
+#include <core/kanacore.hpp>
+#include <core/hw/bus.hpp>
+
+namespace kanacore::hw::me::scratchpad {
+
+using namespace common;
+
+constexpr u64 SCRATCHPAD_ADDR = 0x00000000;
+constexpr u64 SCRATCHPAD_SIZE = 0x200000;
+constexpr u64 SCRATCHPAD_MASK = SCRATCHPAD_SIZE - 1;
+
+static std::array<u8, SCRATCHPAD_SIZE> scratchpad;
+
+template<typename T>
+static T read(const u32 addr) {
+    assert((addr & (sizeof(T) - 1)) == 0);
+
+    T data;
+
+    std::memcpy(&data, scratchpad.data() + (addr & SCRATCHPAD_MASK), sizeof(T));
+
+    return data;
+}
+
+template<typename T>
+static void write(const u32 addr, const T data) {
+    assert((addr & (sizeof(T) - 1)) == 0);
+
+    std::memcpy(scratchpad.data() + (addr & SCRATCHPAD_MASK), &data, sizeof(T));
+}
+
+void initialize() {
+
+}
+
+void soft_reset() {
+
+}
+
+void hard_reset() {
+    const bus::PageDescriptor page_desc {
+        .read8_func   = read<u8>,
+        .read16_func  = read<u16>,
+        .read32_func  = read<u32>,
+        .write8_func  = write<u8>,
+        .write16_func = write<u16>,
+        .write32_func = write<u32>,
+    };
+
+    kanacore::get_me_bus_ptr()->map(SCRATCHPAD_ADDR, SCRATCHPAD_SIZE, page_desc);
+}
+
+void shutdown() {
+
+}
+
+};
