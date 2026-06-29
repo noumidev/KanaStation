ba4a400a02dbeceb1cc4f5d53431289b1589e046
noumi <139501014+noumidev@users.noreply.github.com>
2026-06-29 16:31:48 +0200

    Poll key events and pass to core

--- src/frontends/sdl/main.cpp
+++ src/frontends/sdl/main.cpp
@@ -7,6 +7,8 @@
 
 #define SDL_MAIN_USE_CALLBACKS
 
+#include <unordered_map>
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -27,6 +29,28 @@ static struct {
     SDL_Window* window;
     SDL_Texture* texture;
 } screen;
+
+static const std::unordered_map<SDL_Scancode, kanacore::Button> key_bindings = {
+    { SDL_SCANCODE_W, kanacore::Button::BUTTON_UP       },
+    { SDL_SCANCODE_D, kanacore::Button::BUTTON_RIGHT    },
+    { SDL_SCANCODE_S, kanacore::Button::BUTTON_DOWN     },
+    { SDL_SCANCODE_A, kanacore::Button::BUTTON_LEFT     },
+    { SDL_SCANCODE_I, kanacore::Button::BUTTON_TRIANGLE },
+    { SDL_SCANCODE_J, kanacore::Button::BUTTON_SQUARE   },
+    { SDL_SCANCODE_K, kanacore::Button::BUTTON_CROSS    },
+    { SDL_SCANCODE_L, kanacore::Button::BUTTON_CIRCLE   },
+    { SDL_SCANCODE_U, kanacore::Button::BUTTON_SELECT   },
+    { SDL_SCANCODE_Q, kanacore::Button::BUTTON_L        },
+    { SDL_SCANCODE_E, kanacore::Button::BUTTON_R        },
+    { SDL_SCANCODE_P, kanacore::Button::BUTTON_START    },
+    { SDL_SCANCODE_Z, kanacore::Button::BUTTON_HOME     },
+    { SDL_SCANCODE_N, kanacore::Button::BUTTON_HOLD     },
+    { SDL_SCANCODE_M, kanacore::Button::BUTTON_WLAN_SW  },
+    { SDL_SCANCODE_X, kanacore::Button::BUTTON_VOL_UP   },
+    { SDL_SCANCODE_C, kanacore::Button::BUTTON_VOL_DOWN },
+    { SDL_SCANCODE_V, kanacore::Button::BUTTON_SCREEN   },
+    { SDL_SCANCODE_B, kanacore::Button::BUTTON_NOTE     },
+};
 
 SDL_AppResult SDL_AppInit(void**, int argc, char** argv) {
     if (
@@ -76,6 +100,28 @@ SDL_AppResult SDL_AppEvent(void*, SDL_Event* event) {
     switch (event->type) {
         case SDL_EVENT_QUIT:
             return SDL_APP_SUCCESS;
+        case SDL_EVENT_KEY_DOWN: {
+            // Ignore key repeats
+            if (event->key.repeat) {
+                break;
+            }
+
+            auto it = key_bindings.find(event->key.scancode);
+
+            if (it != key_bindings.end()) {
+                kanacore::press_button(it->second);
+            }
+            break;
+        }
+        case SDL_EVENT_KEY_UP: {
+            auto it = key_bindings.find(event->key.scancode);
+
+            if (it != key_bindings.end()) {
+                kanacore::release_button(it->second);
+            }
+
+            break;
+        }
     }
 
     return SDL_APP_CONTINUE;
